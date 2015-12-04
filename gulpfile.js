21f171e32d8d7db12ec6e2e6836e5cb72bfc66ac
NazirArifin <ceylon.rizan@gmail.com>
2015-12-04 22:16:50 +0700

    ubah package.json dan gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 /* File: gulpfile.js */
-
 var gulp  = require('gulp'),
-	/*jshint = require('gulp-jshint'),*/
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	minify = require('gulp-minify-css'),
