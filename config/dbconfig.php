ec4377473d708d77c04508a2ce2043549d68e918
NazirArifin <ceylon.rizan@gmail.com>
2015-12-04 20:18:00 +0700

    ubah config database

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -3,7 +3,7 @@
 $dbconfig_host = 'localhost';
 
 // The database name to use
-$dbconfig_database = 'amssd';
+$dbconfig_database = '';
 
 // The database authentication user
 $dbconfig_username = 'root';
