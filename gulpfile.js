f6368cc9a00024721250522b0806401419f248ed
NazirArifin <ceylon.rizan@gmail.com>
2016-03-10 11:40:46 +0700

    beri gitignore untuk folder yang kosong

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,48 @@ var gulp  = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
 	flatten = require('gulp-flatten'),
 	watch = require('gulp-watch'),
+	inject = require('gulp-inject'),
+	filter = require('gulp-filter'),
 	livereload = require('gulp-livereload');
+
+/** vendor bower */
+gulp.task('bower', function() {
+	/**
+	 * copy javascript
+	 */
+	gulp.src([
+		'bower_components/jquery/dist/jquery.min.js',
+		'bower_components/bootstrap/dist/js/bootstrap.min.js',
+	]).
+		pipe(flatten()).pipe(gulp.dest('./js'));
+
+	/**
+	 * copy css
+	 */
+	gulp.src([
+		'bower_components/bootstrap/dist/css/bootstrap.min.css',
+		'bower_components/font-awesome/css/font-awesome.min.css',
+	]).
+		pipe(flatten()).pipe(gulp.dest('./css'));
+
+	/**
+	 * copy fonts
+	 */
+	gulp.src([
+		'bower_components/bootstrap/fonts/*.*',
+		'bower_components/font-awesome/fonts/*.*',
+	]).
+		pipe(flatten()).pipe(gulp.dest('./fonts'));
+
+	/**
+	 * concat javascript
+	 */
+	
+	/**
+	 * concat css
+	 */
+	 
+});
 
 /** compile scss in src */
 gulp.task('sass', function() {
@@ -35,7 +76,19 @@ gulp.task('js', function() {
 
 /** copy html in src to view */
 gulp.task('html', function() {
+	var mainFilter = filter('**/index.html', { restore: true });
+
 	return gulp.src('./src/**/*.html').
+		pipe(mainFilter).
+		pipe(inject(gulp.src('./css/*.css', { read: false }))).
+		pipe(inject(gulp.src([
+			'./js/jquery.min.js'
+		], { read: false }), { starttag: '<!-- inject:head:{{ext}} -->' })).
+		pipe(inject(gulp.src([
+			'./js/*.js', 
+			'!./js/jquery.min.js'
+		], { read: false }))).
+		pipe(mainFilter.restore).
 		pipe(flatten()).
 		pipe(gulp.dest('./view')).
 		pipe(livereload());
@@ -43,24 +96,26 @@ gulp.task('html', function() {
 
 /** watch php file in controller and model */
 gulp.task('php', function() {
-	return gulp.src(['./controller/**/*.php', './model/**/*.php']).
+	return gulp.src(['./controller/**/*.php', './model/**/*.php'], { read: false }).
 		pipe(watch(['./controller/**/*.php', './model/**/*.php'])).
 		pipe(livereload());
 });
 
-/** serve task */
-gulp.task('serve', function() {
-
-});
-
 /** watch task */
 gulp.task('watch', function() {
-	//livereload.listen({ quiet: true });
-	livereload.listen({ quiet: false });
-	gulp.watch('./src/**/*.js', ['js']);
-	gulp.watch('./src/**/*.html', ['html']);
-	gulp.watch('./src/**/*.scss', ['sass']);
+	livereload.listen({ quiet: true });
+	// livereload.listen({ quiet: false });
+	
+	// gulp-watch untuk html
+	gulp.src('src/**/*.js', { read: false }).
+		pipe(watch('src/**/*.js', function() { gulp.start('js'); }));
+	// gulp-watch untuk html
+	gulp.src('src/**/*.html', { read: false }).
+		pipe(watch('src/**/*.html', function() { gulp.start('html'); }));
+	// gulp-watch untuk sass
+	gulp.src('src/**/*.scss', { read: false }).
+		pipe(watch('src/**/*.scss', function() { gulp.start('sass'); }));
 });
 
 /** default task */
-gulp.task('default', ['watch', 'php', 'html', 'js', 'sass']);
+gulp.task('default', ['php', 'html', 'js', 'sass', 'watch']);
